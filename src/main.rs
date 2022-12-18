0198299c99427181e1ffc05a8c3c39f1a8bea15a
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2022-12-18 13:34:09 +0200

    Create ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{self, Write};
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    // Render
+
+    let start = Instant::now();
+
+    println!("P3\n{IMAGE_WIDTH}  {IMAGE_HEIGHT}\n255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {j} ");
+        io::stderr().flush().unwrap();
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f64) / (IMAGE_WIDTH - 1) as f64;
+            let g = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u32;
+            let ig = (255.999 * g) as u32;
+            let ib = (255.999 * b) as u32;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
+
+    let duration = start.elapsed();
+    eprint!("\nDone.\nTime elapsed while rendering: {:?}", duration);
 }
