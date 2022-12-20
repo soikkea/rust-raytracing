399723ec08c167260f9d9a60597079c7c588df5c
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2022-12-20 18:24:09 +0200

    Add ray module

---
+++ src/ray.rs
@@ -0,0 +1,28 @@
+use crate::vec3;
+
+#[derive(Debug)]
+pub struct Ray {
+    pub orig: vec3::Point3,
+    pub dir: vec3::Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: vec3::Point3, direction: vec3::Vec3) -> Ray {
+        Ray {
+            orig: origin,
+            dir: direction,
+        }
+    }
+
+    pub fn origin(&self) -> &vec3::Point3 {
+        &self.orig
+    }
+
+    pub fn direction(&self) -> &vec3::Vec3 {
+        &self.dir
+    }
+
+    pub fn at(&self, t: f64) -> vec3::Point3 {
+        self.orig + t * self.dir
+    }
+}
