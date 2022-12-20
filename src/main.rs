be72fe44c9e9b5a7e10ace2807dd6f54a13dbb9a
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2022-12-20 18:24:26 +0200

    Use rays to render

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,34 @@ use std::io::{self, Write};
 use std::time::Instant;
 
 use crate::color::write_color;
-use crate::vec3::Color;
 
 pub mod color;
+pub mod ray;
 pub mod vec3;
+
+fn ray_color(ray: &ray::Ray) -> vec3::Color {
+    let unit_direction = vec3::Vec3::unit_vector(*ray.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * vec3::Color::new(1.0, 1.0, 1.0) + t * vec3::Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = vec3::Point3::new(0.0, 0.0, 0.0);
+    let horizontal = vec3::Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = vec3::Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - vec3::Vec3::new(0.0, 0.0, focal_length);
 
     // Render
 
@@ -23,11 +42,10 @@ fn main() {
         eprint!("\rScanlines remaining: {j} ");
         io::stderr().flush().unwrap();
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                i as f64 / (IMAGE_WIDTH - 1) as f64,
-                j as f64 / (IMAGE_HEIGHT - 1) as f64,
-                0.25,
-            );
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let ray = ray::Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            let pixel_color = ray_color(&ray);
             write_color(&mut io::stdout().lock(), pixel_color).unwrap();
         }
     }
