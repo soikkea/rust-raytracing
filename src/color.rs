a097c9952abfec3783cd3f5dfd018cc27ef0bac6
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2022-12-23 12:41:54 +0200

    Add antialiasing

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,23 @@
 use crate::vec3::Color;
 
-pub fn write_color(out: &mut impl std::io::Write, pixel_color: Color) -> std::io::Result<()> {
-    let r = (255.999 * pixel_color.x()) as u32;
-    let g = (255.999 * pixel_color.y()) as u32;
-    let b = (255.999 * pixel_color.z()) as u32;
-    write!(out, "{} {} {}\n", r, g, b)?;
+pub fn write_color(
+    out: &mut impl std::io::Write,
+    pixel_color: Color,
+    samples_per_pixel: u32,
+) -> std::io::Result<()> {
+    let mut r = pixel_color.x();
+    let mut g = pixel_color.y();
+    let mut b = pixel_color.z();
+
+    let scale = 1.0 / (samples_per_pixel as f64);
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    let r_out = (256.0 * r.clamp(0.0, 0.999)) as u32;
+    let g_out = (256.0 * g.clamp(0.0, 0.999)) as u32;
+    let b_out = (256.0 * b.clamp(0.0, 0.999)) as u32;
+
+    write!(out, "{} {} {}\n", r_out, g_out, b_out)?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use std::io::{self, Write};
 
 use std::time::Instant;
 
+use rand::Rng;
+
 use crate::color::write_color;
 
+pub mod camera;
 pub mod color;
 pub mod hittable;
 pub mod hittable_list;
@@ -27,6 +30,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u32 = 400;
     const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+    const SAMPLES_PER_PIXEL: u32 = 100;
 
     // World
     let mut world = hittable_list::HittableList::new();
@@ -41,15 +45,7 @@ fn main() {
 
     // Camera
 
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = &vec3::Point3::new(0.0, 0.0, 0.0);
-    let horizontal = &vec3::Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = &vec3::Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        &(origin - horizontal / 2.0 - vertical / 2.0 - vec3::Vec3::new(0.0, 0.0, focal_length));
+    let camera = camera::Camera::new();
 
     // Render
 
@@ -61,12 +57,17 @@ fn main() {
         eprint!("\rScanlines remaining: {j} ");
         io::stderr().flush().unwrap();
         for i in 0..IMAGE_WIDTH {
-            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let direction: vec3::Vec3 = lower_left_corner + u * horizontal + v * vertical - origin;
-            let ray = ray::Ray::new(origin, &direction);
-            let pixel_color = ray_color(&ray, &world);
-            write_color(&mut io::stdout().lock(), pixel_color).unwrap();
+            let mut pixel_color = vec3::Color::origin();
+            let mut rng = rand::thread_rng();
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u_rand: f64 = rng.gen();
+                let v_rand: f64 = rng.gen();
+                let u: f64 = (i as f64 + u_rand) / (IMAGE_WIDTH - 1) as f64;
+                let v: f64 = (j as f64 + v_rand) / (IMAGE_HEIGHT - 1) as f64;
+                let ray = camera.get_ray(u, v);
+                pixel_color += ray_color(&ray, &world);
+            }
+            write_color(&mut io::stdout().lock(), pixel_color, SAMPLES_PER_PIXEL).unwrap();
         }
     }
 
